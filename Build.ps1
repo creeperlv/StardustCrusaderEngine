6e334befd13574f40e15c72d4b8844e3f9b610c0
Creeper Lv <creeperlv38@outlook.com>
2021-09-06 02:55:31 +0800

    Added some outputs in the build and package scripts.

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,10 @@
+Write-Output '[Build]Stardust Crusader Engine Build Script'
+Write-Output '[Build]Gathering source files...'
+# Create a temporary directory.
 if(!(Test-Path 'obj')){
     mkdir 'obj'
 }
-dir -i *.java -r -n | Out-file obj/sources.txt
+Get-ChildItem -i *.java -r -n | Out-file obj/sources.txt
+Write-Output '[Build]Building...'
 javac -encoding UTF-8 -cp src -d bin '@obj/sources.txt'
+Write-Output '[Build]Done.'
--- Package.ps1
+++ Package.ps1
@@ -1,3 +1,5 @@
+Write-Output '[Package]Stardust Crusader Engine Pack Script'
+Write-Output '[Package]Starting a build...'
 .\Build.ps1
 if (!(Test-Path 'Build')) {
     mkdir 'Build'
@@ -5,4 +7,8 @@ if (!(Test-Path 'Build')) {
 if ((Test-Path '.\Build\SCE-Latest.jar')) {
     Remove-Item .\Build\SCE-Latest.jar
 }
+Write-Output '[Package]Packaging with JAR'
 jar -c  -f .\Build\SCE-Latest.jar  -C '.\bin\' .\org\
+Write-Output '[Package]Done.'
+$NAME= Get-ChildItem ".\Build\SCE-Latest.jar" | Select-Object FullName
+Write-Output "[Package]Outcome: $NAME"
